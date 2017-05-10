8b04fbcf7fcd5a34a1ae69e277ee2e8db72fc6e6
杨绍伟 <1@qq.com>
2017-05-10 14:01:22 +0800

    launch

--- ui/src/main/java/com/cbs/hzb/ui/activities/MainActivity.java
+++ ui/src/main/java/com/cbs/hzb/ui/activities/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends BaseActivity{
     private DutyFragment dutyFragment;
     private MeFragment meFragment;
 
+    //m
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
